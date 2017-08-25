35d42ca9b9f32c9660174c8e1f022f1212505958
freshgiammi <rengucci.gianmarco@gmail.com>
2017-08-25 18:45:28 +0200

    Bacon: Setup AOSP configuration
    Signed-off-by: freshgiammi <rengucci.gianmarco@gmail.com>

--- aosp_bacon.mk
+++ aosp_bacon.mk
@@ -1,4 +1,5 @@
-# Copyright (C) 2016 The CyanogenMod Project
+#
+# Copyright 2017 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -8,34 +9,24 @@
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or 
+implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+#
 
 # Inherit from bacon device
 $(call inherit-product, device/oneplus/bacon/bacon.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
-PRODUCT_NAME := lineage_bacon
+PRODUCT_NAME := aosp_bacon
 PRODUCT_DEVICE := bacon
-PRODUCT_MANUFACTURER := OnePlus
+PRODUCT_BRAND := Oneplus
 PRODUCT_MODEL := A0001
-
-PRODUCT_GMS_CLIENTID_BASE := android-oneplus
-
-PRODUCT_BRAND := oneplus
-TARGET_VENDOR := oneplus
-TARGET_VENDOR_PRODUCT_NAME := bacon
-TARGET_VENDOR_DEVICE_NAME := A0001
-PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=A0001 PRODUCT_NAME=bacon
-
-# Unofficial build ID
-TARGET_UNOFFICIAL_BUILD_ID := Sultan
+PRODUCT_MANUFACTURER := Oneplus
+PRODUCT_RESTRICT_VENDOR_FILES := false
 
 ## Use the latest approved GMS identifiers unless running a signed build
 ifneq ($(SIGN_BUILD),true)
