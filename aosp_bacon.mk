9c0d7c18fe93a91931705852fc0a8a09c5cb6cf9
freshgiammi <rengucci.gianmarco@gmail.com>
2017-09-02 16:12:38 +0200

    bacon: inherit full_base_telephony.mk
    Signed-off-by: freshgiammi <rengucci.gianmarco@gmail.com>

--- aosp_bacon.mk
+++ aosp_bacon.mk
@@ -18,7 +18,7 @@
 $(call inherit-product, device/oneplus/bacon/bacon.mk)
 
 # Inherit from the common Open Source product configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Get the long list of APNs
 PRODUCT_COPY_FILES += device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
