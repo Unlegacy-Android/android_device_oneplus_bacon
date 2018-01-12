1567fb35f5a707668177c6e62455d04360c282e0
André Pinela <sheffzor@gmail.com>
2018-01-12 09:02:27 +0000

    liblight: Switch to bacon lights
    Change-Id: I102c5c69baf473220ed103c7449fc01dc6bd5f8d
    Signed-off-by: André Pinela <sheffzor@gmail.com>

--- bacon.mk
+++ bacon.mk
@@ -156,7 +156,7 @@ PRODUCT_PACKAGES += \
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-impl \
-    lights.msm8974
+    lights.bacon
 
 # Limit dex2oat threads to improve thermals
 PRODUCT_PROPERTY_OVERRIDES += \
